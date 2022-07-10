a99c911a743958ec20629501d2afeacfe47943ca
Jay Massey <jaymassey98@gmail.com>
2022-07-10 20:45:27 +0100

    initial commit

---
+++ eight-queens-algorithm.cpp
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------
+// This project is a self-taught exercise. For more examples of various coding
+// projects, please see my GitHub portfolio at https://github.com/JayMassey98.
+// ---------------------------------------------------------------------------
+
+#include <iostream>
+
+#include "eight-queens-algorithm.h"
+
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;
+	return 0;
+}
---
+++ eight-queens-algorithm.h
@@ -0,0 +1,10 @@
+﻿// ---------------------------------------------------------------------------
+// This project is a self-taught exercise. For more examples of various coding
+// projects, please see my GitHub portfolio at https://github.com/JayMassey98.
+// ---------------------------------------------------------------------------
+
+// ---------------------------------------
+// PURPOSE: TODO - Add a description here.
+// ---------------------------------------
+
+#pragma once
